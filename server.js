830ee4a82767126b7789bdf23680ba79c3ab62bc
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-29 14:31:55 -0700

    Setup route for the server

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 'use strict';
 console.log('Your Server Is Here!');
 
-
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 3002;
 
-app.get('/test', (request, response) => {
+// Route Section
 
-  response.send('test request received')
+app.get('/', (request, response) => {
 
-})
+  response.send(200).send('This Server is Working');
+
+});
+
+
+
+// Error message section
+app.get('*', (request, response) => {
+  response.status(500).send('"error": "Something went wrong"');
+});
+
+// Catchall (Needs to be at the bottom)
+app.use((error, request, response) => {
+  response.status(500).send(error.message);
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
