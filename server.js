e81fb21b88174cbd2e39222e5edb88e295b11fbc
Stephen Clemmer <stephenjclemmer@gmail.com>
2023-05-21 16:50:36 -0700

    tried to add in dependencies and start mongoose. no success yet

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
-
 db.once('open', function () {
   console.log('Mongoose is connected');
 });
